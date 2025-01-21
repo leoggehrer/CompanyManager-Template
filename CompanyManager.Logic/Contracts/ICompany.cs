1c08c30c7e2c85de4a602cee6df836d6bdcdce2c
leoggehrer <g.gehrer@htl-leonding.ac.at>
2025-01-21 17:18:03 +0100

    Added documentation.

--- CompanyManager.Logic/Contracts/ICompany.cs
+++ CompanyManager.Logic/Contracts/ICompany.cs
@@ -1,8 +1,18 @@
 ﻿namespace CompanyManager.Logic.Contracts
 {
+    /// <summary>
+    /// Represents a company in the company manager.
+    /// </summary>
     public interface ICompany : IIdentifiable
     {
+        /// <summary>
+        /// Gets or sets the name of the company.
+        /// </summary>
         string Name { get; set; }
-        string Address { get; set; }
+        /// <summary>
+        /// Gets or sets the address of the company.
+        /// </summary>
+        string? Address { get; set; }
+
     }
 }
